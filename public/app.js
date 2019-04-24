faf4691b633b8eda464d74fbae65322d467a86d5
Allen M <tmracing008@yahoo.com>
2019-04-24 13:56:42 -0700

    ability to query other currencies, refactored

--- public/app.js
+++ public/app.js
@@ -1,396 +1,392 @@
 const key = "e9PqycN4UPM5OgJpDVHwPKpmvVnx3v";
 
-// bar graph
+$(document).ready(function() {
+  // bar graph
 
-function renderChart(res) {
-  let ctx = $("#myChart");
-  const data = res.Markets;
-  const myChart = new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: [
-        data[0].Name,
-        data[1].Name,
-        data[2].Name,
-        data[3].Name,
-        data[4].Name,
-        data[5].Name,
-        data[6].Name,
-        data[7].Name,
-        data[8].Name,
-        data[9].Name
-      ],
-      datasets: [
-        {
-          label: ["BTC Price"],
-          data: [
-            data[0].Price,
-            data[1].Price,
-            data[2].Price,
-            data[3].Price,
-            data[4].Price,
-            data[5].Price,
-            data[6].Price,
-            data[7].Price,
-            data[8].Price,
-            data[9].Price
-          ],
-          backgroundColor: [
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(66, 134, 244, 0.2)",
-            "rgba(66, 244, 89, 0.2)",
-            "rgba(244, 66, 149, 0.2)",
-            "rgba(229, 66, 244, 0.2)",
-            "rgba(66, 244, 238, 0.2)",
-            "rgba(244, 226, 66, 0.2)",
-            "rgba(244, 66, 66, 0.2)",
-            "rgba(244, 179, 66, 0.2)"
-          ],
-          borderColor: [
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(66, 134, 244, 1)",
-            "rgba(66, 244, 89, 1)",
-            "rgba(244, 66, 149, 1)",
-            "rgba(229, 66, 244, 1)",
-            "rgba(66, 244, 238, 1)",
-            "rgba(244, 226, 66, 1)",
-            "rgba(244, 66, 66, 1)",
-            "rgba(244, 179, 66, 1)"
-          ],
-          borderWidth: 1
-        }
-      ]
-    },
-    options: {
-      legend: {
-        labels: {
-          fontSize: 24
-        }
-      },
-      title: {
-        display: true,
-        text: "Price Bar Graph",
-        fontSize: 32
-      },
-      scales: {
-        yAxes: [
+  function renderChart(res) {
+    let ctx = $("#myChart");
+    const data = res.Markets;
+    let dataLabels = [];
+    let dataPrice = [];
+    for (let i = 0; i < data.length; i++) {
+      dataLabels.push(data[i].Name);
+      dataPrice.push(data[i].Price);
+    }
+    const myChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: dataLabels,
+        datasets: [
           {
-            ticks: {
-              beginAtZero: true,
-              fontSize: 20
-            }
+            label: ["BTC Price"],
+            data: dataPrice,
+            backgroundColor: [
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(66, 134, 244, 0.2)",
+              "rgba(66, 244, 89, 0.2)",
+              "rgba(244, 66, 149, 0.2)",
+              "rgba(229, 66, 244, 0.2)",
+              "rgba(66, 244, 238, 0.2)",
+              "rgba(244, 226, 66, 0.2)",
+              "rgba(244, 66, 66, 0.2)",
+              "rgba(244, 179, 66, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(66, 134, 244, 1)",
+              "rgba(66, 244, 89, 1)",
+              "rgba(244, 66, 149, 1)",
+              "rgba(229, 66, 244, 1)",
+              "rgba(66, 244, 238, 1)",
+              "rgba(244, 226, 66, 1)",
+              "rgba(244, 66, 66, 1)",
+              "rgba(244, 179, 66, 1)"
+            ],
+            borderWidth: 1
           }
         ]
-      }
-    }
-  });
-}
-
-// Render second Graph
-
-function renderGraph(data) {
-  console.log(data);
-  //const data = res
-  let ctx = $("#mySecondChart");
-  const mySecondChart = new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: ["CNY", "EUR", "GBP", "USD"],
-      datasets: [
-        {
-          label: "Price Per BTN",
-          data: [
-            data.Price_cny,
-            data.Price_eur,
-            data.Price_gbp,
-            data.Price_usd
-          ],
-          backgroundColor: [
-            "rgba(11, 117, 5, 0.2)",
-            "rgba(11, 117, 5, 0.2)",
-            "rgba(11, 117, 5, 0.2)",
-            "rgba(11, 117, 5, 0.2)"
-          ],
-          borderColor: [
-            "rgba(11, 117, 5, 1)",
-            "rgba(11, 117, 5, 1)",
-            "rgba(11, 117, 5, 1)",
-            "rgba(11, 117, 5, 1)"
-          ],
-          borderWidth: 1
-        }
-      ]
-    },
-    options: {
-      legend: {
-        labels: {
-          fontSize: 24
-        }
       },
-      title: {
-        display: true,
-        text: "Bitcoin to Market Value",
-        fontSize: 32
-      },
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true,
-              fontSize: 20
+      options: {
+        legend: {
+          labels: {
+            fontSize: 24
+          }
+        },
+        title: {
+          display: true,
+          text: "Price Bar Graph",
+          fontSize: 32
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+                fontSize: 20
+              }
             }
+          ]
+        }
+      }
+    });
+  }
+
+  // second bar graph
+
+  function renderGraph(data) {
+    let ctx = $("#mySecondChart");
+    const mySecondChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["CNY", "EUR", "GBP", "USD"],
+        datasets: [
+          {
+            label: "Price Per BTC",
+            data: [
+              data.Price_cny,
+              data.Price_eur,
+              data.Price_gbp,
+              data.Price_usd
+            ],
+            backgroundColor: [
+              "rgba(11, 117, 5, 0.2)",
+              "rgba(11, 117, 5, 0.2)",
+              "rgba(11, 117, 5, 0.2)",
+              "rgba(11, 117, 5, 0.2)"
+            ],
+            borderColor: [
+              "rgba(11, 117, 5, 1)",
+              "rgba(11, 117, 5, 1)",
+              "rgba(11, 117, 5, 1)",
+              "rgba(11, 117, 5, 1)"
+            ],
+            borderWidth: 1
           }
         ]
-      }
-    }
-  });
-}
-
-// pie graph
-
-function renderPie(res) {
-  let ctx = $("#myThirdChart");
-  const data = res.Markets;
-  const myThirdChart = new Chart(ctx, {
-    type: "pie",
-    data: {
-      labels: [
-        data[0].Name,
-        data[1].Name,
-        data[2].Name,
-        data[3].Name,
-        data[4].Name
-      ],
-      datasets: [
-        {
-          label: ["24h Volume"],
-          data: [
-            data[0].Volume_24h,
-            data[1].Volume_24h,
-            data[2].Volume_24h,
-            data[3].Volume_24h,
-            data[4].Volume_24h
-          ],
-          backgroundColor: [
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(66, 134, 244, 0.2)",
-            "rgba(66, 244, 89, 0.2)",
-            "rgba(244, 66, 149, 0.2)",
-            "rgba(255, 206, 86, 0.2)"
-          ],
-          borderColor: [
-            "rgba(75, 192, 192, 1)",
-            "rgba(66, 134, 244, 1)",
-            "rgba(66, 244, 89, 1)",
-            "rgba(244, 66, 149, 1)",
-            "rgba(255, 206, 86, 1)"
-          ],
-          borderWidth: 1
-        }
-      ]
-    },
-    options: {
-      legend: {
-        labels: {
-          fontSize: 24
-        }
       },
-      title: {
-        display: true,
-        text: "Top 5 BTN 24h",
-        fontSize: 32
-      }
-    }
-  });
-}
-
-function renderRandom(res) {
-  let ctx = $("#myFourthChart");
-  const data = res.Markets;
-  const myFourthChart = new Chart(ctx, {
-    type: "bubble",
-    data: {
-      datasets: [
-        {
-          label: ["BTN"],
-          data: [
+      options: {
+        legend: {
+          labels: {
+            fontSize: 24
+          }
+        },
+        title: {
+          display: true,
+          text: "Bitcoin to Market Value",
+          fontSize: 32
+        },
+        scales: {
+          yAxes: [
             {
-              x: data[0].Volume_24h,
-              y: data[0].Price,
-              r: 10
-            },
-            {
-              x: data[1].Volume_24h,
-              y: data[1].Price,
-              r: 10
-            },
-            {
-              x: data[2].Volume_24h,
-              y: data[2].Price,
-              r: 10
-            },
-            {
-              x: data[3].Volume_24h,
-              y: data[3].Price,
-              r: 10
-            },
-            {
-              x: data[4].Volume_24h,
-              y: data[4].Price,
-              r: 10
-            },
-            {
-              x: data[5].Volume_24h,
-              y: data[5].Price,
-              r: 10
-            },
-            {
-              x: data[6].Volume_24h,
-              y: data[6].Price,
-              r: 10
-            },
-            {
-              x: data[7].Volume_24h,
-              y: data[7].Price,
-              r: 10
-            },
-            {
-              x: data[8].Volume_24h,
-              y: data[8].Price,
-              r: 10
-            },
-            {
-              x: data[9].Volume_24h,
-              y: data[9].Price,
-              r: 10
+              ticks: {
+                beginAtZero: true,
+                fontSize: 20
+              }
             }
-          ],
-          backgroundColor: "rgb(255, 0, 0)"
+          ]
         }
-      ]
-    },
-    options: {
-      legend: {
-        labels: {
-          fontSize: 24
-        }
-      },
-      title: {
-        display: true,
-        text: "Price/Volume",
-        fontSize: 32
       }
+    });
+  }
+
+  // pie graph
+
+  function renderPie(res) {
+    let ctx = $("#myThirdChart");
+    const data = res.Markets;
+    let dataLabels = [];
+    let dataVolume = [];
+    for (let i = 0; i < data.length; i++) {
+      dataLabels.push(data[i].Name);
+      dataVolume.push(data[i].Volume_24h);
     }
-  });
-}
-
-// typewriter constructor
-
-var TxtType = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = "";
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    const myThirdChart = new Chart(ctx, {
+      type: "pie",
+      data: {
+        labels: dataLabels,
+        datasets: [
+          {
+            label: ["24h Volume"],
+            data: dataVolume,
+            backgroundColor: [
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(66, 134, 244, 0.2)",
+              "rgba(66, 244, 89, 0.2)",
+              "rgba(244, 66, 149, 0.2)",
+              "rgba(255, 206, 86, 0.2)"
+            ],
+            borderColor: [
+              "rgba(75, 192, 192, 1)",
+              "rgba(66, 134, 244, 1)",
+              "rgba(66, 244, 89, 1)",
+              "rgba(244, 66, 149, 1)",
+              "rgba(255, 206, 86, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        legend: {
+          labels: {
+            fontSize: 24
+          }
+        },
+        title: {
+          display: true,
+          text: "Top 5 Currencies 24h",
+          fontSize: 32
+        }
+      }
+    });
   }
 
-  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+  // bubble graph
 
-  var that = this;
-  var delta = 200 - Math.random() * 100;
-
-  if (this.isDeleting) {
-    delta /= 2;
+  function renderRandom(res) {
+    let ctx = $("#myFourthChart");
+    const data = res.Markets;
+    let dataBubbles = [];
+    for (let i = 0; i < data.length; i++) {
+      dataBubbles.push({ x: data[i].Volume_24h, y: data[i].Price, r: 10 });
+    }
+    const myFourthChart = new Chart(ctx, {
+      type: "bubble",
+      data: {
+        datasets: [
+          {
+            label: ["Currency"],
+            data: dataBubbles,
+            backgroundColor: "rgb(255, 0, 0)"
+          }
+        ]
+      },
+      options: {
+        legend: {
+          labels: {
+            fontSize: 24
+          }
+        },
+        title: {
+          display: true,
+          text: "Price/Volume",
+          fontSize: 32
+        }
+      }
+    });
   }
 
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === "") {
+  // typewriter constructor
+
+  var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = "";
+    this.tick();
     this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
+  };
 
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
+  TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
 
-window.onload = function() {
-  var elements = document.getElementsByClassName("typewrite");
-  for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute("data-type");
-    var period = elements[i].getAttribute("data-period");
-    if (toRotate) {
-      new TxtType(elements[i], JSON.parse(toRotate), period);
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
-  }
-};
 
-// create dynamic table with api data
+    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
-const createRow = res => {
-  const data = res.Markets;
-  for (let i = 0; i < data.length; i++) {
-    let newRow = $("<tr>").append(
-      $("<td>").text(data[i].Label),
-      $("<td>").text(data[i].Name),
-      $("<td>").text(data[i].Price),
-      $("<td>").text(data[i].Volume_24h),
-      $("<td>").text(moment.unix(data[i].Timestamp).format("lll"))
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) {
+      delta /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+
+  window.onload = function() {
+    var elements = document.getElementsByClassName("typewrite");
+    for (var i = 0; i < elements.length; i++) {
+      var toRotate = elements[i].getAttribute("data-type");
+      var period = elements[i].getAttribute("data-period");
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+  };
+
+  // dynamic table with api data
+
+  const createRow = res => {
+    const data = res.Markets;
+    for (let i = 0; i < data.length; i++) {
+      let newRow = $("<tr>").append(
+        $("<td>").text(data[i].Label),
+        $("<td>").text(data[i].Name),
+        $("<td>").text(data[i].Price),
+        $("<td>").text(data[i].Volume_24h),
+        $("<td>").text(moment.unix(data[i].Timestamp).format("lll"))
+      );
+      $("#table-data").append(newRow);
+    }
+  };
+
+  // dopdown dynamic selection for currency
+
+  const dropdown = res => {
+    console.log(res);
+    for (let i = 0; i < res.length; i++) {
+      let orginalLabel = res[i].Label;
+      let regexLabel = orginalLabel.replace("/", "");
+      let newLabel = regexLabel.toLowerCase();
+      let option = $("<option>");
+      option.attr("value", newLabel);
+      option.text(res[i].Name);
+      $("#dropdown").append(option);
+    }
+  };
+
+  // Load data from API
+
+  let queryURL =
+    "https://www.worldcoinindex.com/apiservice/ticker?key=" +
+    key +
+    "&label=ethbtc-ltcbtc-omnibtc-cajbtc-xmrbtc-dashbtc-ppcbtc-bnbbtc-metbtc-eosbtc&fiat=btc";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(res) {
+    createRow(res);
+    renderChart(res);
+    renderRandom(res);
+  });
+
+  let secondQueryUrl =
+    "https://www.worldcoinindex.com/apiservice/json?key=" + key;
+  $.ajax({
+    url: secondQueryUrl,
+    method: "GET"
+  }).then(function(res) {
+    const data = res.Markets[290];
+    renderGraph(data);
+    const filterResponse = res.Markets;
+    const fiveThousandths = filterResponse.filter(
+      res => res.Price_btc >= 0.005
     );
-    $("#table-data").append(newRow);
-  }
-};
+    const removeDashBtc = fiveThousandths.filter(res => res.Name !== "Dash");
+    const removeLitecoin = removeDashBtc.filter(res => res.Name !== "Litecoin");
+    const removeEhereum = removeLitecoin.filter(res => res.Name !== "Ethereum");
+    const removeExistingBtc = removeEhereum.filter(
+      res => res.Name !== "Monero"
+    );
+    dropdown(removeExistingBtc);
+  });
 
-// Load data from API
+  let thirdQueryUrl =
+    "https://www.worldcoinindex.com/apiservice/ticker?key=" +
+    key +
+    "&label=ethbtc-ltcbtc-btcbtc-eosbtc-bchbtc&fiat=btc";
+  $.ajax({
+    url: thirdQueryUrl,
+    method: "GET"
+  }).then(function(res) {
+    renderPie(res);
+  });
 
-let queryURL =
-  "https://www.worldcoinindex.com/apiservice/ticker?key=" +
-  key +
-  "&label=ethbtc-ltcbtc-omnibtc-cajbtc-xmrbtc-dashbtc-ppcbtc-bnbbtc-metbtc-eosbtc&fiat=btc";
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(res) {
-  createRow(res);
-  renderChart(res);
-  renderRandom(res);
-});
+  // onClick listener for dropdown, api call
 
-let secondQueryUrl =
-  "https://www.worldcoinindex.com/apiservice/json?key=" + key;
-$.ajax({
-  url: secondQueryUrl,
-  method: "GET"
-}).then(function(res) {
-  const data = res.Markets[290];
-  console.log(data);
-  renderGraph(data);
-});
-
-let thirdQueryUrl =
-  "https://www.worldcoinindex.com/apiservice/ticker?key=" +
-  key +
-  "&label=ethbtc-ltcbtc-btcbtc-eosbtc-bchbtc&fiat=btc";
-$.ajax({
-  url: thirdQueryUrl,
-  method: "GET"
-}).then(function(res) {
-  renderPie(res);
+  $("#submit").on("click", function(e) {
+    e.preventDefault();
+    console.log("clicked");
+    function validate() {
+      let isValid = true;
+      $(".form-control").each(function() {
+        if ($(this).val() === "Select an Option") {
+          isValid = false;
+        }
+      });
+      return isValid;
+    }
+    if (validate()) {
+      let newQuery = $(".form-control")
+        .find(":selected")
+        .val();
+      console.log(newQuery);
+      let queryURL =
+        "https://www.worldcoinindex.com/apiservice/ticker?key=" +
+        key +
+        "&label=" +
+        newQuery +
+        "&fiat=btc";
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(res) {
+        createRow(res);
+        // renderChart(res);
+        // renderRandom(res);
+      });
+    } else {
+      $("#validation-modal").modal("toggle");
+    }
+  });
 });
